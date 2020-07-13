4a124a23de8a830d74f156b7dd926b796ca7fa27
Taekhyang <xorgid1195@gmail.com>
2020-07-13 16:41:40 +0900

    This is MySQL [connection, cursor] python module to be imported by mysql_connection.py

--- app/test/mysql_connection.py
+++ app/test/mysql_connection.py
@@ -3,28 +3,45 @@ import tornado.ioloop
 import tornado.web
 import sys
 import asyncio
-import mysql.connector
+
+from app.module import dbModule
+# I made a separate directory only for MySQL connection python module
 
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     # The default has changed from selector to proactor in Python 3.8.
     # Thus, this line should be added to detour probable errors
 
-def mysql_connect():
-    #to connect mysql_database
-    cnx = mysql.connector.connect(
-                #connect to mysql server
-                host='127.0.0.1',
-                password='your_password',
-                user='root',
-                port=3307,
-                database='mydatabase'
-            )
-    mycursor = cnx.cursor()
+def insert(data_input):
+    # MySQL INSERT function
+    db_class = dbModule.Database()
 
-    return cnx, mycursor
-cnx, mycursor = mysql_connect()
-#cnx, mycursor variables are going to be used in the handler classes
+    sql = "INSERT INTO test_table (test_data) VALUES (%s)"
+    val = (data_input,)
+
+    db_class.execute(sql, val)
+    #insert input data into test_data
+    #structure (schema : 'mydatabase' -> table : 'test_table' -> column : 'test_data')
+    db_class.commit()
+    #to apply changes and commit
+    print(db_class.mycursor.rowcount, "record inserted.")
+
+def select(data_input):
+    # MySQL SELECT function
+    db_class = dbModule.Database()
+
+    sql = "SELECT * FROM test_table WHERE test_id = %s"
+    id = (data_input,)
+    db_class.execute(sql, id)
+
+    row = db_class.mycursor.fetchall()
+    print(row)
+    value_in_id = row[0][1]
+    #row structure --> [(x, y)] tuple in list, so I refered to y by row[0][1]
+    print(value_in_id)
+
+    return value_in_id
+
 
 class DataInsertHandler(tornado.web.RequestHandler):
 # to get data input by html form and transmit input data into MySQL server to save it
@@ -41,17 +58,10 @@ class DataInsertHandler(tornado.web.RequestHandler):
         # to set header type
 
         data_input = self.get_argument("message")
+        insert(data_input)
 
         self.write("you have saved data :  " + data_input + "  to MySQL server 'mydatabase'")
 
-        sql = "INSERT INTO test_table (test_data) VALUES (%s)"
-        val = (data_input,)
-        mycursor.execute(sql, val)
-        #insert input data into test_data
-        #structure (schema : 'mydatabase' -> table : 'test_table' -> column : 'test_data')
-        cnx.commit()
-        #to apply changes and commit
-        print(mycursor.rowcount, "record inserted.")
 
 
 class DataSelectHandler(tornado.web.RequestHandler):
@@ -63,26 +73,15 @@ class DataSelectHandler(tornado.web.RequestHandler):
                    '<input type="submit" value="Submit">'
                    '</form></body></html>')
 
+
     def post(self):
         #to get data from Mysql database("mydatabase") and show it on the page "/showdata"
         self.set_header("Content-Type", "text/plain")
 
         data_input = self.get_argument("message")
+        value_in_id = select(data_input)
 
-
-        sql = "SELECT * FROM test_table WHERE test_id = %s"
-        id = (data_input,)
-        mycursor.execute(sql, id)
-        #only show test_table values according to test_id which has been input on the "/showdata" page
-
-        myresult = mycursor.fetchall()
-        import itertools
-        out = list(itertools.chain(*myresult))
-        #change tupel type to list type
-        id_value = out[1]
-        #out[0] is input id and out[1] is the value of it
-        self.write(id_value)
-        #to write data in "mydatabase" matched to input id
+        self.write(value_in_id)
 
 application = tornado.web.Application([
     (r"/", DataInsertHandler),
