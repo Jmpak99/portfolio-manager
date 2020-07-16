013f1ad73246d9a0486dc7bb0562980fbeb15b60
Taekhyang <xorgid1195@gmail.com>
2020-07-16 12:22:53 +0900

    "password" has been changed, default value has been changed from args={} to args=None

--- app/module/MySQL_conn_Module.py
+++ app/module/MySQL_conn_Module.py
@@ -7,7 +7,7 @@ class Database():
         self.cnx = mysql.connector.connect(
             # connect to mysql server
             host='127.0.0.1',
-            password='um8910vs',
+            password='your_password',
             user='root',
             port=3307,
             database='mydatabase'
