e81496986b293e0458520d9e2e807d76fed32697
Taekhyang <xorgid1195@gmail.com>
2020-07-16 12:24:48 +0900

    select_from_table(), show_columns_from_table() have been moved to MySQL_conn_Module

--- app/test/Bootstrap_MySQL_table.py
+++ app/test/Bootstrap_MySQL_table.py
@@ -3,7 +3,7 @@ import tornado.ioloop
 import tornado.web
 import sys
 import asyncio
-from app.module import db_connection_Module
+from app.module import MySQL_conn_Module
 # I made a separate directory only for MySQL connection python module
 
 if sys.platform == 'win32':
@@ -12,51 +12,25 @@ if sys.platform == 'win32':
     # Thus, this line should be added to detour probable errors
 
 
-def select_from_table():
-    # to get all table record data by SELECT method
-    db_class = db_connection_Module.Database()
-
-    sql = "SELECT * FROM test_table"
-
-    db_class.execute(sql)
-
-    row = db_class.mycursor.fetchall()
-
-    return row
-
-
-def show_columns_from_table():
-    # to take a name of the each column from the table
-    db_class = db_connection_Module.Database()
-
-    sql = "SHOW COLUMNS FROM test_table"
-
-    db_class.execute(sql)
-
-    rows = db_class.mycursor.fetchall()
-
-    columns = []
-
-    for row in rows:
-        columns.append(row[0])
-        # to save column name in the "columns" list
-    return columns
-
-
 class DataTableShowHandler(tornado.web.RequestHandler):
     # to show data in the schema as a table form
     def get(self):
-        columns = show_columns_from_table()
+        db_table = MySQL_conn_Module.db_Table()
+        # to use db_Table() methods
+        columns = db_table.show_columns_from_table()
         # "contents" has a list of tuples which has table data
         # (ex. : [(id_value1, address_value1), (id_value2, address_value2)...]
-        contents = select_from_table()
+        contents = db_table.select_from_table()
+
+
 
         self.render("bootstrap_table.html",
-                    database_name = "show table",
-                    table_name = "Test Table",
-                    contents = contents,
-                    columns = columns
+                    database_name="show table",
+                    table_name="Test Table",
+                    contents=contents,
+                    columns=columns
                     )
+
 
 application = tornado.web.Application([
     (r"/show-all", DataTableShowHandler)
