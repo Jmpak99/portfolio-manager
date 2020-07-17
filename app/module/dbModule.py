cb1df8fdc3cf5494883a07078285f16419d86c50
Taekhyang <xorgid1195@gmail.com>
2020-07-18 03:06:25 +0900

    - insert_into_db(), select_by_id() have been merged into Database() class

--- app/module/dbModule.py
+++ app/module/dbModule.py
@@ -5,7 +5,6 @@ class Database():
     def __init__(self):
         # to connect mysql_database
         self.cnx = mysql.connector.connect(
-            # connect to mysql server
             host='127.0.0.1',
             password='your_password',
             user='root',
@@ -30,37 +29,34 @@ class Database():
     def commit(self):
         self.cnx.commit()
 
-def insert_into_db(data_input):
-    # MySQL INSERT function
-    db_class = Database()
+    def insert_into_db(self, data_input):
+        # MySQL INSERT function
+        db_class = Database()
 
-    sql = "INSERT INTO test_table (test_data) VALUES (%s)"
+        sql = "INSERT INTO test_table (test_data) VALUES (%s)"
 
-    val = (data_input,)
+        val = (data_input,)
+        # In Python, a tuple containing a single value must include a comma.
+        # For example, ('abc') is evaluated as a scalar while ('abc',) is evaluated as a tuple.
+        db_class.execute(sql, val)
+        #insert input data into test_data
+        #structure (schema : 'mydatabase' -> table : 'test_table' -> column : 'test_data')
 
-    db_class.execute(sql, val)
-    #insert input data into test_data
-    #structure (schema : 'mydatabase' -> table : 'test_table' -> column : 'test_data')
+        db_class.commit()
 
-    db_class.commit()
-    #to apply changes and commit
+    def select_by_id(self, id_input):
+        # when id number is input, it returns data matched to the input id using MySQL SELECT function
+        db_class = Database()
 
-    print(db_class.mycursor.rowcount, "record inserted.")
+        sql = "SELECT * FROM test_table WHERE test_id = %s"
 
+        id = (id_input,)
 
-def select_by_id(id_input):
-    # when id number is input, it returns data matched to the input id using MySQL SELECT function
-    db_class = Database()
+        db_class.execute(sql, id)
 
-    sql = "SELECT * FROM test_table WHERE test_id = %s"
+        row = db_class.mycursor.fetchall()
 
-    id = (id_input,)
+        value_in_id = row[0][1]
+        #row structure --> [(x, y)] tuple in list, so I refered to y by row[0][1]
 
-    db_class.execute(sql, id)
-
-    row = db_class.mycursor.fetchall()
-
-    value_in_id = row[0][1]
-    #row structure --> [(x, y)] tuple in list, so I refered to y by row[0][1]
-
-    return value_in_id
+        return value_in_id
