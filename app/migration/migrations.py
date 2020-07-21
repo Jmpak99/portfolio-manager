a0bd36f39eec4d743570b11b29d6eca742f20355
taekhyang <xorgid1195@gmail.com>
2020-07-21 20:57:30 +0900

    declaration of db has been moved outside create_test_table()

--- app/migration/migrations.py
+++ app/migration/migrations.py
@@ -5,16 +5,15 @@ MIGRATION_HEAD = int(input())
 
 # input 0 to create table "test_table" with columns "test_id", "test_data"
 
+db = db_module.Database()
+# connection and cursor should be reusable
 
 def create_test_table():
-    db = db_module.Database()
-
-    query = "CREATE TABLE test_table_2 (test_id INT AUTO_INCREMENT PRIMARY KEY, test_data VARCHAR(255))"
+    query = "CREATE TABLE test_table (test_id INT AUTO_INCREMENT PRIMARY KEY, test_data VARCHAR(255))"
 
     db.execute(query)
 
     db.commit()
-
 
 MIGRATIONS = [
     create_test_table
