bb50e2b3f6a0cc72ee7a5b9fed9c978e4f848710
taekhyang <xorgid1195@gmail.com>
2020-07-23 17:24:14 +0900

    db_module.py has been combined to db_query_module.py

--- app/module/db_query_module.py
+++ app/module/db_query_module.py
@@ -30,6 +30,38 @@ class Database:
     def commit(self):
         self.cnx.commit()
 
+    def insert_into_db(self, data_input):
+        # MySQL INSERT function
+        db_class = Database()
+
+        sql = "INSERT INTO test_table (test_data) VALUES (%s)"
+
+        val = (data_input,)
+        # In Python, a tuple containing a single value must include a comma.
+        # For example, ('abc') is evaluated as a scalar while ('abc',) is evaluated as a tuple.
+        db_class.execute(sql, val)
+        # insert input data into test_data
+        # structure (schema : 'mydatabase' -> table : 'test_table' -> column : 'test_data')
+
+        db_class.commit()
+
+    def select_by_id(self, id_input):
+        # when id number is input, it returns data matched to the input id using MySQL SELECT function
+        db_class = Database()
+
+        sql = "SELECT * FROM test_table WHERE test_id = %s"
+
+        id = (id_input,)
+
+        db_class.execute(sql, id)
+
+        row = db_class.mycursor.fetchall()
+
+        value_in_id = row[0][1]
+        # row structure --> [(x, y)] tuple in list, so I refered to y by row[0][1]
+
+        return value_in_id
+
 
 def show_columns_from_table():
     # to take a name of the each column from the table
