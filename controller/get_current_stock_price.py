08c4cd385585575b5f31b86a209b8a2239dc0703
taekhyang <xorgid1195@gmail.com>
2020-07-26 15:07:14 +0900

    - get one parameter "stock ticker symbol" and automatically decide whethere it is south korea or united states

--- controller/get_current_stock_price.py
+++ controller/get_current_stock_price.py
@@ -1,9 +1,20 @@
 import investpy
 import json
+import re
 
 
-# get parameters (stock ticker symbol, country name)
-def get_current_stock_price(stock_symbol, country):
+# get parameter (stock ticker symbol), and decide whether the country is south korea or united states
+def get_current_stock_price(stock_symbol):
+    # south korea ticker symbol starts with a number while that of United states starts with an alphabet
+    x = re.search("[0-9]", stock_symbol)
+
+    if x:
+        country = "south korea"
+
+    else:
+        country = "united States"
+
+
     current_price_raw = investpy.stocks.get_stock_recent_data(stock_symbol, country, as_json=True, order='descending', interval='Daily')
 
     # to change dictionary embedded str type to dictionary type
