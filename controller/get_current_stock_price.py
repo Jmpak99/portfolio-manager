1d3e89e422b7eda65bfaee1c0a3f1f30663e032b
taekhyang <xorgid1195@gmail.com>
2020-07-26 13:53:58 +0900

    - get_current_stock_price.py has been created - to get current stock price

--- controller/get_current_stock_price.py
+++ controller/get_current_stock_price.py
@@ -2,14 +2,15 @@ import investpy
 import json
 
 
+# get parameters (stock ticker symbol, country name)
 def get_current_stock_price(stock_symbol, country):
-    # param1 = stock ticker symbol, param2 = country name
     current_price_raw = investpy.stocks.get_stock_recent_data(stock_symbol, country, as_json=True, order='descending', interval='Daily')
 
-    current_price_dict = json.loads(current_price_raw)
     # to change dictionary embedded str type to dictionary type
+    current_price_dict = json.loads(current_price_raw)
 
-    current_price = current_price_dict['recent'][0]['close']
     # get today's close price for a certain stock
+    current_price = current_price_dict['recent'][0]['close']
+
 
     return current_price
